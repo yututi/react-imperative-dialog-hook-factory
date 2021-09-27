49b846cc732f6511e2826ed9436b2382e7ab2b3a
yututi <y.tsuchiya0207@gmail.com>
2021-09-28 00:54:37 +0900

    change sample output dir.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const config = {
   entry: "./src/SampleApp.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   devServer: {
     open: true,
